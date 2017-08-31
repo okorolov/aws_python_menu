cb9861e328ebe24008979456d625b2eb8c4a048b
Oleksii Korolov <oleksii.korolov@harmonicinc.com>
2017-08-31 13:18:13 +0300

    Added comments to .py file, added DryRun check for create function

--- aws_python_menu.py
+++ aws_python_menu.py
@@ -2,15 +2,18 @@
 import boto3
 from botocore.exceptions import ClientError
 
+# Function to check status of all instances
 def aws_status():
     ec2 = boto3.client('ec2')
     response = ec2.describe_instances()
     print(response)
 
+# Function to start an aws instance
 def aws_start():
     print('Enter instance id')
     instance_id = input()
     ec2 = boto3.client('ec2')
+    # Do a dryrun to verify permissions
     try:
         ec2.start_instances(InstanceIds=[instance_id], DryRun=True)
     except ClientError as e:
@@ -23,6 +26,7 @@ def aws_start():
     except ClientError as e:
         print(e)
 
+# Function to start an aws instance
 def aws_stop():
     print('Enter instance id')
     instance_id = input()
@@ -33,13 +37,14 @@ def aws_stop():
     except ClientError as e:
         if 'DryRunOperation' not in str(e):
             raise
-    # Dry run succeeded, call stop_instances witout dryrun
+    # Dry run succeeded, call stop_instances without dryrun
     try:
         response = ec2.stop_instances(InstanceIds=[instance_id], DryRun=False)
         print(response)
     except ClientError as e:
         print(e)
 
+# Function to restart an aws instance
 def aws_restart():
     print('Enter instance id')
     instance_id = input()
@@ -50,21 +55,31 @@ def aws_restart():
     except ClientError as e:
         if 'DryRunOperation' not in str(e):
             raise
-    # Dry run succeeded, call stop_instances witout dryrun
+    # Dry run succeeded, call stop_instances without dryrun
     try:
         response = ec2.stop_instances(InstanceIds=[instance_id], DryRun=False)
         print(response)
     except ClientError as e:
         print(e)
 
+# Function to create an aws instance
+# This will create only Ubuntu 16 t2.micro image and start it
 def aws_create_instance():
     ec2 = boto3.resource('ec2')
+    # Do a dryrun first to verify permissions
     try:
-        instance = ec2.create_instances(ImageId='ami-785db401',InstanceType='t2.micro',MaxCount=1,MinCount=1)
+        ec2.create_instances(ImageId='ami-785db401', InstanceType='t2.micro', MaxCount=1, MinCount=1, DryRun=True)
+    except ClientError as e:
+        if 'DryRunOperation' not in str(e):
+            raise
+    # If DryRun succeeded run creation of an instance
+    try:
+        instance = ec2.create_instances(ImageId='ami-785db401', InstanceType='t2.micro', MaxCount=1, MinCount=1)
         print(instance)
     except ClientError as e:
         print(e)
 
+# Function for menu
 def menu():
     while True:
         print('1. Get information about instances')
@@ -92,6 +107,5 @@ def menu():
 
 print('Welcome to Amazon Control')
 print('Select action:')
-
 
 menu()
